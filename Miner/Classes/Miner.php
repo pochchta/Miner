5a271862da60ccec1aa55b53b273f98daeff7e62
pochchta <pochchta@rambler.ru>
2020-04-28 14:41:12 +0800

    алгоритм поиска соседних пустых ячеек не работает по диагонали

--- Miner/Classes/Miner.php
+++ Miner/Classes/Miner.php
@@ -7,8 +7,20 @@ class Miner
 	private $field = array(array());
 	private $messages = array();
 	private $endGame = false;
+	private $startGame = false;
+	private $countEmptyCell;
+	private $width;
+	private $height;
+	private $numberBombs;
+	const MESSAGE_LOSE_GAME = 'Вы проиграли.';
+	const MESSAGE_WIN_GAME = 'Поздравляем. Вы победили.';
+	const MESSAGE_END_GAME = 'Игра закончена. Начните новую.';
 	public function __construct($height, $width, $numberBombs)
 	{
+		$this->width = $width;
+		$this->height = $height;
+		$this->numberBombs = $numberBombs;
+		$this->countEmptyCell = $width * $height - $numberBombs;
 		for ($h = 0; $h < $height; $h++){
 			for ($w = 0; $w < $width; $w++){
 				$this->field[$h][$w] = new EmptyCell();
@@ -43,18 +55,65 @@ class Miner
 		}
 	}
 	public function isBomb($h, $w)
-	{		
-		$cell = $this->field[$h][$w];
-		if ($cell instanceof Cell) {
-			$cell->visible = true;
+	{
+		if ($this->isEndGame() == false) {
+			$cell = $this->field[$h][$w];
+			while ($cell instanceof BombCell && $this->startGame == false) {
+				self::__construct($this->height, $this->width, $this->numberBombs);
+				$cell = $this->field[$h][$w];
+			}
+			if ($cell instanceof BombCell) {
+				$cell->exploded = true;
+				$this->endGame = true;
+				$this->messages[] = self::MESSAGE_LOSE_GAME;
+			}
+			if ($this->isEndGame() == false) {
+				if ($cell instanceof Cell && $cell->visible == false) {
+					$this->startGame = true;
+					$cell->visible = true;
+					$this->countEmptyCell--;
+					$this->openBesideCell($h, $w);
+				}
+			}
 		}
-		if ($cell instanceof BombCell) {
-			$cell->exploded = true;
+		if ($this->countEmptyCell == 0) {
 			$this->endGame = true;
-			$this->messages[] = 'Вы проиграли';
-			return true;
+			$this->messages[] = self::MESSAGE_WIN_GAME;
+		}		
+		if ($this->isEndGame() == true) {
+			$this->messages[] = self::MESSAGE_END_GAME;
+		}	
+	}
+	private function openBesideCell($h, $w)
+	{
+		$paths = array(
+			array('h' => -1, 'w' => 0),
+			array('h' => 1, 'w' => 0),
+			array('h' => 0, 'w' => -1),
+			array('h' => 0, 'w' => 1),
+		);
+		foreach ($paths as $path) {
+			$hNew = $h + $path['h'];
+			$wNew = $w + $path['w'];
+			if (
+				$wNew < 0 || 
+				$hNew < 0 || 
+				$wNew >= $this->width || 
+				$hNew >= $this->height
+			) {
+				continue;
+			}
+			if (
+				$this->field[$hNew][$wNew] instanceof EmptyCell &&
+				$this->field[$hNew][$wNew]->visible == false
+			) {
+				$this->field[$hNew][$wNew]->visible = true;
+				$this->countEmptyCell--;
+				if ($this->field[$hNew][$wNew]->countBombAround == 0) {
+					$this->openBesideCell($hNew, $wNew);
+				}
+			}
 		}
-		return false;
 	}
 	public function getMessages()
 	{
